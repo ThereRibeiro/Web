1b327dee02e5713d43da4b2ece33c5c78b451606
ThereRibeiro <teregaren089@gmail.com>
2026-09-04 19:36:11 -0300

    arr html

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ console.log("fui clicado");
 let texto = botao.querySelector("span");
 texto.textContent++;
 }
+const btnTemaEscuro = document.querySelector(".btn-tema escuro");
+btnTemaEscuro.addEventListener("Click", MudaTema);
+function Muda Tema() {
+    const
